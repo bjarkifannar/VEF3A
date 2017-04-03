0c74c09fdb9e3a12fe52e0593051829d59687ec7
bjarkifannar <bjarkifannar@gmail.com>
2017-04-03 13:27:55 +0000

    Lagaði vandamál með Verkefni 6

--- Verkefni6/resources/views/welcome.blade.php
+++ Verkefni6/resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title></title>
-	</head>
-	<body>
-		<ul>
-			@foreach ($tasks as $task)
-				<li>{{ $task->body }}</li>
-			@endforeach
-		</ul>
-	</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<h1>Welcome!</h1>
+@endsection
